9a0d2ebc1c04a4df8396d1cc1cc1b15b6d4274a7
Milena Lara <milenalrf@gmail.com>
2021-11-01 16:42:51 -0300

    Inputs controlados e requisição para criar endereço

--- src/pages/CreateAddressPage/CreateAddressForm.js
+++ src/pages/CreateAddressPage/CreateAddressForm.js
@@ -1,42 +1,89 @@
 import React from 'react';
 import { ScreenContainer } from './CreateAddressPageStyles';
+import useForm from '../../Hooks/useForm';
+import { useHistory } from 'react-router';
+import axios from 'axios';
+import { base_url } from '../../constants/urls';
 // import { Button, TextField, Typography } from '@mui/material';
 
 const CreateAddressForm = () => {
+  const [form, onChange, clear] = useForm({
+    street: "",
+    number: "",
+    complement: "",
+    neighbourhood: "",
+    city: "",
+    state: ""
+  })
+
+  const history = useHistory();
+
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+    addAddress()
+  }
+
+
+  const addAddress = () => {
+    axios
+      .post(`${base_url}/labe-food3/address`, {
+        headers:
+        {
+          "auth": localStorage.getItem("token"),
+          "Content-Type": "application/json"
+        }
+      }, form)
+      .then(response => {
+        alert("Endereço adiconado com sucesso");
+        //colocar função de redirecionar para login
+        clear();
+      })
+      .catch(error => {
+        alert("Desculpe, houve um erro");
+        console.log(error)
+      })
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmitForm}>
       <ScreenContainer>
         <input
+          name="street"
           placeholder="Logradouro"
           type="text"
           required
         />
         <input
+          name="number"
           placeholder="Número"
           type="text"
           required
         />
         <input
+          name="complement"
           placeholder="Complemento"
           type="text"
           required
         />
         <input
+          name="neighbourhood"
           placeholder="Bairro"
           type="text"
           required
         />
         <input
+          name="city"
           placeholder="Cidade"
           type="text"
           required
         />
         <input
+          name="state"
           placeholder="Estado"
           type="text"
           required
         />
-        <button>salvar</button>
+        <button type="submit">salvar</button>
         {/* <Typography variant="subtitle1" gutterBottom component="div">
          <strong>Meu endereço</strong>
        </Typography>
--- src/pages/CreateAddressPage/CreateAddressPage.js
+++ src/pages/CreateAddressPage/CreateAddressPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ScreenContainer as div } from './CreateAddressPageStyles';
 import CreateAddressForm from './CreateAddressForm';
 // import { Button, TextField, Typography } from '@mui/material';
 
