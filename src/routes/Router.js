f247ce2a4db7e76c9b1421d8da4785aa95bf9083
Carlos Daniel Oliveira Nunes <carlosdaniel44u@gmail.com>
2021-11-01 16:47:48 -0300

    criação de rotas

---
+++ src/routes/Router.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage";
+import CreateAddressPage from "../pages/CreateAddressPage/CreateAddressPage";
+import EditAddressPage from "../pages/EditAddressPage/EditAddressPage";
+import EditProfilePage from "../pages/EditProfilePage/EditProfilePage";
+import FeedPage from "../pages/FeedPage/FeedPage";
+import RestaurantPage from "../pages/RestaurantPage/RestaurantPage";
+import ChartPage from "../pages/ChartPage/ChartPage";
+import ProfilePage from "../pages/ProfilePage/ProfilePage";
+
+const Router = () => {
+  <BrowserRouter>
+    <Switch>
+      <Route exact path={"/login"}>
+        <LoginPage />
+      </Route>
+
+      <Route exact path={"/signUp"}>
+        <SignUpPage />
+      </Route>
+
+      <Route exact path={"/create-address"}>
+        <CreateAddressPage />
+      </Route>
+
+      <Route exact path={"/edit-address"}>
+        <EditAddressPage />
+      </Route>
+
+      <Route exact path={"/edit-profile"}>
+        <EditProfilePage />
+      </Route>
+
+      <Route exact path={"/feed"}>
+        <FeedPage />
+      </Route>
+
+      <Route exact path={"/restaurant-page"}>
+        <RestaurantPage />
+      </Route>
+
+      <Route exact path={"/chart"}>
+        <ChartPage />
+      </Route>
+
+      <Route exact path={"/profile-page"}>
+        <ProfilePage />
+      </Route>
+    </Switch>
+  </BrowserRouter>;
+};
+
+export default Router;
