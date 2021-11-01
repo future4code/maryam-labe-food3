483e387d41bc268730a37c0a11b6be59c6574b9b
fabiofa87 <fabiofadev87@gmail.com>
2021-11-01 12:37:51 -0300

    Criado useForm e paginas de login/signup

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import useForm from "./Hooks/useForm";
 
 const LoginPage = () => {
+  const [form, onChange, clearForm] = useForm({
+    email: "",
+    password: "",
+  });
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log(form);
+    clearForm();
+  };
   return (
     <div>
-      Login
+      <h3>Login Page</h3>
+      <form onSubmit={onSubmitForm}>
+        <label>Username:</label>
+        <input
+          onChange={onChange}
+          name={"email"}
+          value={form.name}
+          type="text"
+          placeholder="Enter your email"
+        />
+        <br />
+        <label>Password:</label>
+        <input
+          onChange={onChange}
+          name={"password"}
+          value={form.password}
+          type="password"
+          placeholder="Enter your password"
+          title="Senha precisa ter minimo de 8 caracteres e pelo menos 1 letra e 1 numero"
+          pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+        />
+        <br />
+        <button type="submit">Entrar</button>
+      </form>
     </div>
   );
-}
+};
 
 export default LoginPage;
--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React from "react";
+import useForm from "./Hooks/useForm";
 
 const SignUpPage = () => {
+  const [form, onChange, clearForm] = useForm({
+    name: "",
+    email: "",
+    cpf: "",
+    password: "",
+  });
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log(form);
+    clearForm();
+  };
   return (
     <div>
       Cadastro
+      <form onSubmit={onSubmitForm}>
+        <label>Nome</label>
+        <input
+          onChange={onChange}
+          name={"name"}
+          value={form.name}
+          type="text"
+        />
+        <br />
+        <label>E-mail</label>
+        <input
+          onChange={onChange}
+          name={"email"}
+          value={form.email}
+          type="email"
+        />
+        <br />
+        <label>CPF</label>
+        <input
+          onChange={onChange}
+          name={"cpf"}
+          value={form.cpf}
+          type="text"
+          title="Insira um CPF valido"
+          pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})"
+        ></input>
+        <br />
+        <label>Senha</label>
+        <input
+          onChange={onChange}
+          name={"password"}
+          value={form.passoword}
+          type="password"
+          id="txtPassword"
+          title="Senha precisa ter minimo de 8 caracteres e pelo menos 1 letra e 1 numero"
+          placeholder="Enter Password"
+          required
+          pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+        />
+        <button type="submit">Criar</button>
+      </form>
     </div>
   );
-}
+};
 
 export default SignUpPage;
