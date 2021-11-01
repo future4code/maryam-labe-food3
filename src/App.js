61a568c5955d394076706d102c57122a48a7e559
Milena Lara <milenalrf@gmail.com>
2021-11-01 15:53:14 -0300

    Deletando o material

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { ThemeProvider } from '@emotion/react';
-import { theme } from './constants/theme';
 import CreateAddressPage from './pages/CreateAddressPage/CreateAddressPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <div>
       Oi
-    </ThemeProvider>
+    </div>
   );
 }
 
