857aa8576599c88f29065c789439dd5b7f34be93
Milena Lara <milenalrf@gmail.com>
2021-11-01 17:23:41 -0300

    Card da pg de restaurante

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CreateAddressPage from './pages/CreateAddressPage/CreateAddressPage';
 import LoginPage from './pages/LoginPage/LoginPage';
+import RestaurantCard from './components/RestaurantCard/RestaurantCard';
 
 function App() {
   return (
     <div>
-      Oi
+      <RestaurantCard />
     </div>
   );
 }
--- src/components/RestaurantCard/RestaurantCard.js
+++ src/components/RestaurantCard/RestaurantCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { CardContainer, ImgWrapper } from './RestaurantCardStyles';
 
 const RestaurantCard = () => {
   return (
-    <div>
-      Restaurante card
-    </div>
+    <CardContainer>
+      <ImgWrapper src="https://s2.glbimg.com/9zc9T-9LwXwKG_8XOq_9EF67bSQ=/620x455/e.glbimg.com/og/ed/f/original/2021/04/30/receita-hamburguer-smash-burguer-bacon-cheddaar.jpg" />
+      <div>
+        <h5>Bullger</h5>
+        <p>Pão, carne, queijo, picles e molho</p>
+        <h6>R$20,00</h6>
+        <button>adicionar</button>
+      </div>
+    </CardContainer>
   );
 }
 
