840f999c7ab5128c46ed2c7a6d7bde503a02d7b8
Milena Lara <milenalrf@gmail.com>
2021-11-01 12:53:08 -0300

    A importação do useForms estava com path errada, consertei

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './constants/theme';
 import CreateAddressPage from './pages/CreateAddressPage/CreateAddressPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      {/* Oi */}
-      <CreateAddressPage />
+      Oi
     </ThemeProvider>
   );
 }
