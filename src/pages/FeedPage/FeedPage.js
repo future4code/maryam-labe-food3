0637657019590b92d1a8ec91dc43ea6dc451c734
Elida Nunes Cardoso <elidanccardoso@gmail.com>
2021-11-02 18:56:37 -0300

    filtros

--- src/pages/FeedPage/FeedPage.js
+++ src/pages/FeedPage/FeedPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useProtectedPage from "../../Hooks/useProtectedPage"
 import useRequestData from "../../Hooks/useRequestData"
 import Footer from "../../components/Footer/Footer"
@@ -8,17 +8,30 @@ import { DivRestaurants, DivImg } from "./FeedPageStyles";
 
 const FeedPage = () => {
   // useProtectedPage();
+  const [ search , setSearch ] = useState("");
+  const [ categorySearch , setCategorySearch] = useState("");
   
   const data = useRequestData({}, `${base_url}/fourFoodA/restaurants`);
   const restaurants = data.restaurants
 
-  const restaurantCategory = restaurants && restaurants.map((restaurant) => {
+  const restaurantCategory = restaurants && restaurants
+  .filter((restaurant) => {
+    return restaurant.category.toLowerCase() === restaurant.category.toLowerCase()
+  })
+  .map((restaurant) => {
     return (
-      <option value={restaurant.category} key={restaurant.id}>{restaurant.category}</option>
+      <button onClick={() => handleCategory(restaurant.category)} key={restaurant.id}>{restaurant.category}</button>
     )
   });
 
-  const restaurantComponents = restaurants && restaurants.map((restaurant) => {
+  const restaurantComponents = restaurants && restaurants
+  .filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(search.toLowerCase())
+  })
+  .filter((restaurant) => {
+    return restaurant.category.toLowerCase().includes(categorySearch.toLowerCase())
+  })
+  .map((restaurant) => {
     return (
       <DivRestaurants key={restaurant.id}>
         <DivImg>
@@ -34,18 +47,22 @@ const FeedPage = () => {
     )
   });
 
-  
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const handleCategory = (value) => {
+    setCategorySearch(value)
+  }
 
   return (
     <div>
       <div>
-        <input placeholder={"Restaurante"}/>
+        <input placeholder={"Restaurante"} onChange={handleSearch}/>
       </div>
       <div>
         <p>Opções Restaurante:</p>
-        <select>
           {restaurantCategory}
-        </select>
       </div>
       <div>
         {restaurantComponents}
