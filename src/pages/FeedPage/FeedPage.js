002d559b6b30e3445c250437ff72cd78265b8a1c
Elida Nunes Cardoso <elidanccardoso@gmail.com>
2021-11-01 21:03:03 -0300

    cards restaurantes

--- src/pages/FeedPage/FeedPage.js
+++ src/pages/FeedPage/FeedPage.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import useProtectedPage from "../../Hooks/useProtectedPage"
+import useRequestData from "../../Hooks/useRequestData"
 import Footer from "../../components/Footer/Footer"
+import {base_url} from "../../constants/urls"
+
+import { DivRestaurants, DivImg } from "./FeedPageStyles";
 
 const FeedPage = () => {
+  // useProtectedPage();
+  
+  const data = useRequestData({}, `${base_url}/fourFoodA/restaurants`);
+  const restaurants = data.restaurants
+
+  const restaurantCategory = restaurants && restaurants.map((restaurant) => {
+    return (
+      <option value={restaurant.category} key={restaurant.id}>{restaurant.category}</option>
+    )
+  });
+
+  const restaurantComponents = restaurants && restaurants.map((restaurant) => {
+    return (
+      <DivRestaurants key={restaurant.id}>
+        <DivImg>
+          <img src={restaurant.logoUrl}/>
+        </DivImg>
+        <p>{restaurant.name}</p>
+        {restaurant.deliveryTime <= 20 ? 
+          <p>{restaurant.deliveryTime} min</p> : 
+          <p>{restaurant.deliveryTime - 10} - {restaurant.deliveryTime} min</p>
+        }
+        <p>Frete: R$:{restaurant.shipping},00</p>
+      </DivRestaurants>
+    )
+  });
+
+  
+
   return (
     <div>
       <div>
@@ -10,13 +44,12 @@ const FeedPage = () => {
       <div>
         <p>Opções Restaurante:</p>
         <select>
-          <option>Buger</option>
-          <option>Asiática</option>
-          <option>Massas</option>
-          <option>Saudáveis</option>
+          {restaurantCategory}
         </select>
       </div>
-      {/* Fazer requisição com base nas options */}
+      <div>
+        {restaurantComponents}
+      </div>
       <Footer />
     </div>
   );
