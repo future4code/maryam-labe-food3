210832768530364caa5ce3ad25f31750b2531ae5
Milena Lara <milenalrf@gmail.com>
2021-11-03 11:15:17 -0300

    reboot

--- src/constants/urls.js
+++
@@ -1,1 +0,0 @@
-export const base_url = "https://us-central1-missao-newton.cloudfunctions.net";
--- src/pages/CreateAddressPage/CreateAddressPageStyles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const ScreenContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 1rem;
-`
