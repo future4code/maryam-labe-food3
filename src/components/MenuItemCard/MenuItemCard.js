be3dec7ffb8a5130b2c92794168c0c1b4f060b25
Milena Lara <milenalrf@gmail.com>
2021-11-03 17:02:38 -0300

    Revert "Header do restaurante e cards dos produtos funcionando"

--- src/components/MenuItemCard/MenuItemCard.js
+++ src/components/MenuItemCard/MenuItemCard.js
@@ -2,24 +2,19 @@ import React from 'react';
 import { Typography } from '@mui/material';
 import { CardContainer, ImgWrapper } from './MenuItemCardStyles';
 
-const MenuItemCard = ({ product }) => {
-  const currency = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  });
-
+const MenuItemCard = ({ restaurant }) => {
   return (
     <CardContainer sx={{ maxWidth: 345, boxShadow: 1 }}>
-      <ImgWrapper src={product.photoUrl} />
+      <ImgWrapper src="https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_960_720.jpg" />
       <div>
         <Typography variant="subtitle2" gutterBottom component="div" color="primary">
-          <strong>{product.name}</strong>
+          <strong>{restaurant.name && restaurant.name}</strong>
         </Typography>
         <Typography variant="body2" gutterBottom component="div" color="secondary">
-          {product.description}
+          Pão, carne, queijo
         </Typography>
         <Typography variant="subtitle2" gutterBottom component="div">
-          <strong>{currency.format(product.price)}</strong>
+          <strong>R$20,00</strong>
         </Typography>
       </div>
       {/* <ImgWrapper src="https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_960_720.jpg" />
--- src/components/RestaurantCard/RestaurantCard.js
+++ src/components/RestaurantCard/RestaurantCard.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import { CardContainer, ImgWrapper, TextWrapper } from './RestaurantCardStyles';
-import { Typography } from '@mui/material';
+import { CardContainer, ImgWrapper } from './RestaurantCardStyles';
 
-const RestaurantCard = ({ restaurant }) => {
-  const currency = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  });
-
+const RestaurantCard = () => {
   return (
-    <CardContainer>
-      <ImgWrapper src={restaurant.logoUrl} />
-      <Typography variant="subtitle2" gutterBottom component="div" color="primary">
-        <strong>{restaurant.name}</strong>
-      </Typography>
-      <Typography variant="body2" gutterBottom component="div" color="secondary">
-        {restaurant.category}
-      </Typography>
-      <TextWrapper>
-        <Typography variant="body2" gutterBottom component="div" color="secondary">
-          {restaurant.deliveryTime} min
-        </Typography>
-        <Typography variant="body2" gutterBottom component="div" color="secondary">
-          Frete {currency.format(restaurant.shipping)}
-        </Typography>
-      </TextWrapper>
-      <Typography variant="body2" gutterBottom component="div" color="secondary">
-        {restaurant.address}
-      </Typography>
-    </CardContainer>
+    <div>
+
+    </div>
   );
 }
 
