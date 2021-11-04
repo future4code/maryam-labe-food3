77f695c1f1345fb35f900e3adc8a46ef9ce5054a
Milena Lara <milenalrf@gmail.com>
2021-11-04 06:54:50 -0300

    Página do login estilizada

---
+++ src/pages/LoginPage/LoginPageStyles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 4rem 1rem 0 1rem;
+`
