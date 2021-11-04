742fd2724f3f64c0128ad0939d0f31964a619e70
Elida Nunes Cardoso <elidanccardoso@gmail.com>
2021-11-04 17:21:57 -0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { DivFooter, DivButtons, PaperStyled } from "./FooterStyles";
+import { DivButtons, PaperStyled } from "./FooterStyles";
 import { useHistory } from "react-router";
 import { goToFeed, goToCart, goToProfile } from "../../routes//coordinator";
 
@@ -13,55 +13,50 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 const Footer = () => {
   const history = useHistory();
   const [toggleHome, setToggleHome] = useState(true);
-  const [toggleCart, setToggleCart] = useState(false);
-  const [toggleProfile, setToggleProfile] = useState(false);
+  const [toggleCart, setToggleCart] = useState(true);
+  const [toggleProfile, setToggleProfile] = useState(true);
 
-  const onClickButton = (goTo, setState) => {
+  const onClickButton = (goTo, state, setState) => {
+    setState(!state);
+    setToggleHome(false)
     goTo(history);
-    setState(true);
-    setToggleHome(!toggleHome)
   }
 
-  useEffect(() => {
-
-  }, [onClickButton]);
+  console.log(toggleProfile)
 
   return (
     <PaperStyled sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
       <BottomNavigation showLabels>
         <DivButtons>
           {toggleHome ? 
-              <IconButton color="primary" aria-label="add to shopping cart">
-                <HomeOutlinedIcon fontSize="large" onClick={() => onClickButton(goToFeed, setToggleHome)}/>
+              <IconButton color="primary" aria-label="add to shopping cart" onClick={() => onClickButton(goToFeed, toggleHome, setToggleHome)}>
+                <HomeOutlinedIcon fontSize="large" />
               </IconButton> 
                 :
-              <IconButton color="secondary" aria-label="add to shopping cart">
-                <HomeOutlinedIcon fontSize="large" onClick={() => onClickButton(goToFeed, setToggleHome)}/>
+              <IconButton color="secondary" aria-label="add to shopping cart" onClick={() => onClickButton(goToFeed, toggleHome, setToggleHome)}>
+                <HomeOutlinedIcon fontSize="large" />
               </IconButton> 
           }
           {toggleCart ? 
-              <IconButton color="primary" aria-label="add to shopping cart">
-                <ShoppingCartOutlinedIcon fontSize="large" onClick={() => onClickButton(goToCart, setToggleCart)}/>
+              <IconButton color="primary" aria-label="add to shopping cart" onClick={() => onClickButton(goToCart, toggleCart, setToggleCart)}>
+                <ShoppingCartOutlinedIcon fontSize="large" />
               </IconButton> 
                 :
-              <IconButton color="secondary" aria-label="add to shopping cart">
-                <ShoppingCartOutlinedIcon fontSize="large" onClick={() => onClickButton(goToCart, setToggleCart)}/>
+              <IconButton color="secondary" aria-label="add to shopping cart" onClick={() => onClickButton(goToCart, toggleCart, setToggleCart)}>
+                <ShoppingCartOutlinedIcon fontSize="large" />
               </IconButton> 
           }
           {toggleProfile ? 
-              <IconButton color="primary" aria-label="add to shopping cart">
-                <PersonOutlineOutlinedIcon fontSize="large" onClick={() => onClickButton(goToProfile, setToggleProfile)}/>
+              <IconButton color="primary" aria-label="add to shopping cart" onClick={() => onClickButton(goToProfile, toggleProfile, setToggleProfile)}>
+                <PersonOutlineOutlinedIcon fontSize="large" />
               </IconButton> 
                 :
-              <IconButton color="secondary" aria-label="add to shopping cart">
-                <PersonOutlineOutlinedIcon fontSize="large" onClick={() => onClickButton(goToProfile, setToggleProfile)}/>
+              <IconButton color="secondary" aria-label="add to shopping cart" onClick={() => onClickButton(goToProfile, toggleProfile, setToggleProfile)}>
+                <PersonOutlineOutlinedIcon fontSize="large" />
               </IconButton> 
           }
-        {/* <IconButton color="primary" aria-label="add to shopping cart">
-          <PersonOutlineOutlinedIcon fontSize="large" onClick={() => goToProfile(history)}/>
-        </IconButton> */}
         </DivButtons>
-        </BottomNavigation>
+      </BottomNavigation>
     </PaperStyled>
   );
 };
